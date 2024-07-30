019ee8ba0ee7d9868fb6c538f7fc9e3b9651dfed
dura12 <duranu104@gmail.com>
2024-07-30 12:42:34 +0300

    Update index.js

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -10,16 +10,26 @@ function addTask() {
     const li_item = document.createElement('li');
     const span = document.createElement('span');
     const del_button = document.createElement('button');
+    const editt = document.createElement('button');
+
     span.textContent = input.value.trim();
     del_button.textContent = 'Remove';
+    editt.textContent = "Edit";
     li_item.appendChild(span);
     li_item.appendChild(del_button);
+    li_item.append(editt)
     taskList.appendChild (li_item);
+
     input.value = "";
 
     del_button.addEventListener('click', () => {
      li_item.remove();
     });
+    editt.addEventListener('click', () => {
+      let ans = prompt("enter a new task")
+      if (ans != ""){
+      span.textContent = ans;}
+     });
 
 
   }
